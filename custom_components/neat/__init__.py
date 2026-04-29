8553f3c83c6576915a306356058261977ae2bf1c
attar-ohad <123019188+attar-ohad@users.noreply.github.com>
2026-04-29 13:02:10 +0300

    Update __init__.py

--- custom_components/neat/__init__.py
+++ custom_components/neat/__init__.py
@@ -1,1 +1,22 @@
+from .const import DOMAIN
+from .coordinator import NeatCoordinator
 
+async def async_setup_entry(hass, entry):
+    hass.data.setdefault(DOMAIN, {})
+
+    coordinator = NeatCoordinator(
+        hass,
+        entry.data["token"],
+        entry.data["org_id"],
+        entry.data["devices"],
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(
+        entry, ["sensor", "binary_sensor"]
+    )
+
+    return True
